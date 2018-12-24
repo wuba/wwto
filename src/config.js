9485e01ef580d59bc0a31e14fcb91b1b58040762
chenyufu <chenyufu@58ganji.com>
2018-12-24 11:40:42 +0800

    exclude .json from static files

--- src/config.js
+++ src/config.js
@@ -4,7 +4,8 @@ module.exports =  {
       src + "/**/*.*",
       "!" + src + "/**/*.wxml",
       "!" + src + "/**/*.wxss",
-      "!" + src + "/**/*.js"
+      "!" + src + "/**/*.js",
+      "!" + src + "/**/*.json"
     ];
   }
 };
