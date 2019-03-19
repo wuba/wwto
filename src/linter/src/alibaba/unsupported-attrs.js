311be4859769d1a700d61b24c01ecbdffa7660ea
chenyu15 <chenyu15@58ganji.com>
2019-03-19 10:49:22 +0800

    整理阿里 组件缺失属性&缺失事件到js文件

--- src/linter/src/alibaba/unsupported-attrs.js
+++ src/linter/src/alibaba/unsupported-attrs.js
@@ -11,5 +11,166 @@ module.exports = [
     attrs: [
       'aria-label'
     ]
+  },
+  {
+    tag: 'swiper',
+    attrs: [
+      'current-item-id',
+      'display-multiple-items',
+      'skip-hidden-item-layout'
+    ]
+  },
+  {
+    tag: 'movable-view',
+    attrs: [
+      'inertia',
+      'out-of-bounds',
+      'damping',
+      'friction',
+      'scale',
+      'scale-min',
+      'scale-max',
+      'scale-value',
+      'animation'
+    ]
+  },
+  {
+    tag: 'movable-area',
+    attrs: [
+      'scale-area'
+    ]
+  },
+  {
+    tag: 'cover-view',
+    attrs: [
+      'scroll-top',
+      'aria-role',
+      'aria-label'
+    ]
+  },
+  {
+    tag: 'cover-image',
+    attrs: [
+      'aria-role',
+      'aria-label'
+    ]
+  },
+  {
+    tag: 'icon',
+    attrs: [
+      'aria-label'
+    ]
+  },
+  {
+    tag: 'text',
+    attrs: [
+      'space',
+      'decode'
+    ]
+  },
+  {
+    tag: 'rich-text',
+    attrs: [
+      'space'
+    ]
+  },
+  {
+    tag: 'progress',
+    attrs: [
+      'border-radius',
+      'font-size',
+      'color',
+      'active-mode',
+      'aria-label'
+    ]
+  },
+  {
+    tag: 'button',
+    attrs: [
+      'lang',
+      'session-from',
+      'send-message-title',
+      'send-message-path',
+      'send-message-img',
+      'show-message-card'
+    ]
+  },
+  {
+    tag: 'checkbox',
+    attrs: [
+      'aria-label'
+    ]
+  },
+  {
+    tag: 'input',
+    attrs: [
+      'cursor-spacing',
+      'auto-focus',
+      'adjust-position',
+      'aria-label'
+    ]
+  },
+  {
+    tag: 'picker',
+    attrs: [
+      'mode'
+    ]
+  },
+  {
+    tag: 'radio',
+    attrs: [
+      'aria-label'
+    ]
+  },
+  {
+    tag: 'slider',
+    attrs: [
+      'color'
+    ]
+  },
+  {
+    tag: 'switch',
+    attrs: [
+      'type'
+    ]
+  },
+  {
+    tag: 'textarea',
+    attrs: [
+      'auto-focus',
+      'fixed',
+      'cursor-spacing',
+      'cursor',
+      'show-confirm-bar',
+      'selection-start',
+      'selection-end',
+      'adjust-position',
+      'aria-label'
+    ]
+  },
+  {
+    tag: 'navigator',
+    attrs: [
+      'target',
+      'delta',
+      'app-id',
+      'path',
+      'extra-data',
+      'version',
+      'hover-stop-propagation'
+    ]
+  },
+  {
+    tag: 'map',
+    attrs: [
+      'covers',
+      'subkey',
+      'enable-3D',
+      'show-compass',
+      'enable-overlooking',
+      'enable-zoom',
+      'enable-scroll',
+      'enable-rotate'
+    ]
   }
 ];
--- src/linter/src/alibaba/unsupported-tags.js
+++ src/linter/src/alibaba/unsupported-tags.js
@@ -1,158 +1,159 @@
 module.exports = [
 
-//  view
-//  zfb 缺失属性
-//  aria-role, aria-label
+  //  view
+  //  zfb 缺失属性
+  //  aria-role, aria-label
 
-//  scroll-view
-//  zfb 缺失属性
-//  aria-label
-//  zfb 事件对应
-//  bindscrolltoupper => onScrollToUpper
-//  bindscrolltolower => onScrollToLower
-//  bindscroll => onScroll
+  //  scroll-view
+  //  zfb 缺失属性
+  //  aria-label
+  //  zfb 事件对应
+  //  bindscrolltoupper => onScrollToUpper
+  //  bindscrolltolower => onScrollToLower
+  //  bindscroll => onScroll
 
-//  swiper
-//  zfb 缺失属性
-//  current-item-id,display-multiple-items,skip-hidden-item-layout
-//  zfb 事件对应
-//  bindchange => onChange
-//  zfb 缺失事件
-//  bindtransition,bindanimationfinish
-//  swiper-item 缺失item-id
+  //  swiper
+  //  zfb 缺失属性
+  //  current-item-id,display-multiple-items,skip-hidden-item-layout
+  //  zfb 事件对应
+  //  bindchange => onChange
+  //  zfb 缺失事件
+  //  bindtransition,bindanimationfinish
+  //  swiper-item 缺失item-id
 
-//  movable-view
-//  zfb 缺失属性
-//  inertia,out-of-bounds,damping,friction,scale,scale-min,scale-max,scale-value,animation
-//  zfb 事件对应
-//  bindchange=>onChange
-//  缺失事件
-//  bindscale,htouchmove,vtouchmove
+  //  movable-view
+  //  zfb 缺失属性
+  //  inertia,out-of-bounds,damping,friction,scale,scale-min,scale-max,scale-value,animation
+  //  zfb 事件对应
+  //  bindchange=>onChange
+  //  缺失事件
+  //  bindscale,htouchmove,vtouchmove
 
-//  movable-area
-//  缺失属性
-//  scale-area
+  //  movable-area
+  //  缺失属性
+  //  scale-area
 
-//  cover-view
-//  缺失属性
-//  scroll-top,aria-role,aria-label
+  //  cover-view
+  //  缺失属性
+  //  scroll-top,aria-role,aria-label
 
-//  cover-image
-//  缺失属性
-//  aria-role,aria-label
-//  缺失事件
-//  bindload,binderror
+  //  cover-image
+  //  缺失属性
+  //  aria-role,aria-label
+  //  缺失事件
+  //  bindload,binderror
 
-//  icon
-//  缺失属性
-//  aria-label
+  //  icon
+  //  缺失属性
+  //  aria-label
 
-//  text
-//  缺失属性
-//  space,decode
+  //  text
+  //  缺失属性
+  //  space,decode
 
-//  rich-text
-//  缺失属性
-//  space
+  //  rich-text
+  //  缺失属性
+  //  space
 
-//  progress
-//  缺失属性
-//  border-radius,font-size,color,active-mode,aria-label
-//  对应属性
-//  activeColor -> active-color
-//  backgroundColor -> background-color
-//  缺失事件
-//  bindactiveend
+  //  progress
+  //  缺失属性
+  //  border-radius,font-size,color,active-mode,aria-label
+  //  对应属性
+  //  activeColor -> active-color
+  //  backgroundColor -> background-color
+  //  缺失事件
+  //  bindactiveend
 
-//  button
-//  缺失属性
-//  lang,session-from,send-message-title,send-message-path,send-message-img,show-message-card
-//  app-parameter,aria-label
-//  缺失事件
-//  bindgetuserinfo,bindcontact,bindgetphonenumber,binderror,bindlaunchapp,bindopensetting
-//  open-type 不同
-//  缺失 contact,getUserInfo,getPhoneNumber,openSetting,feedback
+  //  button
+  //  缺失属性
+  //  lang,session-from,send-message-title,send-message-path,send-message-img,show-message-card
+  //  app-parameter,aria-label
+  //  缺失事件
+  //  bindgetuserinfo,bindcontact,bindgetphonenumber,binderror,bindlaunchapp,bindopensetting
+  //  open-type 不同
+  //  缺失 contact,getUserInfo,getPhoneNumber,openSetting,feedback
 
-//  checkbox-group
-//  事件对应
-//  bindchange =>onChange
+  //  checkbox-group
+  //  事件对应
+  //  bindchange =>onChange
 
-//  checkbox
-//  缺失属性
-//  aria-label
+  //  checkbox
+  //  缺失属性
+  //  aria-label
 
-//  form
-//  对应事件
-//  bindsubmit =>onSubmit
-//  bindreset =>onReset
+  //  form
+  //  对应事件
+  //  bindsubmit =>onSubmit
+  //  bindreset =>onReset
 
-//  input
-//  缺失属性
-//  cursor-spacing,auto-focus,adjust-position,aria-label
-//  对应事件
-//  bindinput => onInput
-//  bindfocus => onFocus
-//  bindblur => onBlur
-//  bindconfirm => onConfirm
+  //  input
+  //  缺失属性
+  //  cursor-spacing,auto-focus,adjust-position,aria-label
+  //  对应事件
+  //  bindinput => onInput
+  //  bindfocus => onFocus
+  //  bindblur => onBlur
+  //  bindconfirm => onConfirm
 
 
-//  label=label
+  //  label=label
 
-//  picker
-//  缺失属性 mode 只是普通选择器
-//  对应事件
-//  bindchange=>onChange
-//  缺失事件
-//  bindcancel
+  //  picker
+  //  缺失属性 mode 只是普通选择器
+  //  对应事件
+  //  bindchange=>onChange
+  //  缺失事件
+  //  bindcancel
 
-//  picker-view
-//  对应事件
-//  bindchange => onChange
-//  缺失事件
-//  bindpickstart,bindpickend
+  //  picker-view
+  //  对应事件
+  //  bindchange => onChange
+  //  缺失事件
+  //  bindpickstart,bindpickend
 
-//  radio-group
-//  对应事件
-//  bindchange=>onChange
+  //  radio-group
+  //  对应事件
+  //  bindchange=>onChange
 
-//  radio
-//  缺失属性
-//  aria-label
+  //  radio
+  //  缺失属性
+  //  aria-label
 
-//  slider
-//  缺失属性
-//  color
-//  对应属性
-//  activeColor->active-color
-//  backgroundColor->background-color
-//  block-size->handle-size
-//  block-color->handle-color
-//  对应事件
-//  bindchange=>onChange
-//  bindchanging=>onChanging
+  //  slider
+  //  缺失属性
+  //  color
+  //  对应属性
+  //  activeColor->active-color
+  //  backgroundColor->background-color
+  //  block-size->handle-size
+  //  block-color->handle-color
+  //  对应事件
+  //  bindchange=>onChange
+  //  bindchanging=>onChanging
 
-//  switch
-//  缺失属性
-//  type
-//  对应事件
-//  bindchange=>onChange
+  //  switch
+  //  缺失属性
+  //  type
+  //  对应事件
+  //  bindchange=>onChange
 
-//  textarea
-//  缺失属性
-//  auto-focus,fixed,cursor-spacing,cursor,show-confirm-bar,selection-start,selection-end,adjust-position,aria-label
-//  对应事件
-//  bindfocus=>onFocus
-//  bindblur=>onBlur
-//  bindinput=>onInput
-//  bindconfirm=>onConfirm
-//  缺失事件
-//  bindlinechange
+  //  textarea
+  //  缺失属性
+  //  auto-focus,fixed,cursor-spacing,cursor,show-confirm-bar
+  //  selection-start,selection-end,adjust-position,aria-label
+  //  对应事件
+  //  bindfocus=>onFocus
+  //  bindblur=>onBlur
+  //  bindinput=>onInput
+  //  bindconfirm=>onConfirm
+  //  缺失事件
+  //  bindlinechange
 
-//  navigator
-//  缺失属性
-//  target,delta,app-id,path,extra-data,version,hover-stop-propagation
-//  缺失事件
-//  bindsuccess,bindfail,bindcomplete
+  //  navigator
+  //  缺失属性
+  //  target,delta,app-id,path,extra-data,version,hover-stop-propagation
+  //  缺失事件
+  //  bindsuccess,bindfail,bindcomplete
 
   'functional-page-navigator',
   'audio',
@@ -161,40 +162,41 @@ module.exports = [
   'live-player',
   'live-pusher',
 
-//  image
-//  对应事件
-//  binderror=>onError
-//  bindload=>onLoad
+  //  image
+  //  对应事件
+  //  binderror=>onError
+  //  bindload=>onLoad
 
-//  map
-//  缺失属性
-//  covers,subkey,enable-3D,show-compass,enable-overlooking,enable-zoom,enable-scroll,enable-rotate
-//  对应事件
-//  bindmarkertap	=> onMarkerTap
-//  bindcallouttap => onCalloutTap
-//  bindcontroltap => onControlTap
-//  bindregionchange => onRegionChange
-//  bindtap => onTap
-//  无对应事件
-//  bindupdated,bindpoitap
-//  markers
-//  缺失属性
-//  zIndex
-//  markers 对应事件
-//  anchor -> anchorX,anchorY
-//
+  //  map
+  //  缺失属性
+  //  covers,subkey,enable-3D,show-compass
+  //  enable-overlooking,enable-zoom,enable-scroll,enable-rotate
+  //  对应事件
+  //  bindmarkertap => onMarkerTap
+  //  bindcallouttap => onCalloutTap
+  //  bindcontroltap => onControlTap
+  //  bindregionchange => onRegionChange
+  //  bindtap => onTap
+  //  无对应事件
+  //  bindupdated,bindpoitap
+  //  markers
+  //  缺失属性
+  //  zIndex
+  //  markers 对应事件
+  //  anchor -> anchorX,anchorY
+  //
 
-//  canvas
-//  对应属性
-//  canvas-id -> id
-//  对应事件
-//  bindtouchstart => onTouchStart
-//  bindtouchmove => onTouchMove
-//  bindtouchend => onTouchEnd
-//  bindtouchcancel => onTouchCancel
-//  bindlongtap => onLongTap
-//  缺失事件
-//  binderror
+  //  canvas
+  //  对应属性
+  //  canvas-id -> id
+  //  对应事件
+  //  bindtouchstart => onTouchStart
+  //  bindtouchmove => onTouchMove
+  //  bindtouchend => onTouchEnd
+  //  bindtouchcancel => onTouchCancel
+  //  bindlongtap => onLongTap
+  //  缺失事件
+  //  binderror
 
   //web-view
   //缺失事件
