0c59e2b74a90363c05cab1e3d65ed92324dd7379
qiaochunmei <qiaochunmei@58ganji.com>
2019-03-19 16:12:18 +0800

    将头条、百度组件缺失属性整理到.js文件

--- src/linter/src/baidu/unsupported-tags.js
+++ src/linter/src/baidu/unsupported-tags.js
@@ -2,17 +2,14 @@ module.exports = [
   //// 视图容器
   // view
 
-  // 缺失属性  百度中无aria-role,aria-label属性
   // 注意事项  百度小程序从基础库版本1.12.0开始支持事件捕获、冒泡。
 
   // scroll-view
 
-  // 缺失属性  百度中无enable-back-to-top,aria-label属性
   // 注意事项  微信小程序中属性upper-threshold、lower-threshold、scroll-top、scroll-left单位px，2.4.0起支持rpx,百度小程序中单位是px
 
   // swiper
 
-  // 缺失属性  百度中无skip-hidden-item-layout属性
   // 缺失事件  百度中无bindtransition事件
   // 注意事项
   // 1.indicator-active-color属性默认值不同,微信中是#000000,百度中是#333
@@ -27,7 +24,6 @@ module.exports = [
 
   // cover-view
 
-  // 缺失属性  百度中无scroll-top、aria-role、aria-label属性
   // 注意事项
   // 1.微信中该组件覆盖在原生组件之上的文本视图，可覆盖的原生组件包括map、video、canvas、camera、live-player、live-pusher，只支持嵌套cover-view、cover-image，
   // 可在cover-view中使用button。
@@ -35,14 +31,12 @@ module.exports = [
 
   // cover-image
 
-  // 缺失属性  百度中无aria-role、aria-label属性
   // 注意事项
   // 1.该组件Bug & Tips部分微信和百度小程序不太一样,具体请参考组件说明部分
 
   ////基础内容
   // icon
 
-  // 缺失属性  百度中无aria-label属性
   // 差异属性  size属性在微信中类型是Number / String,单位px（2.4.0起支持rpx）,百度中类型是是Number,单位是 px
   // 注意事项
   // 1.该组件属性size类型在百度中是Number类型,在微信中是Number/String
@@ -50,7 +44,6 @@ module.exports = [
 
   // text
 
-  // 缺失属性  百度中无decode属性
   // 注意事项
   // 1.该组件Tips说明部分百度没有指出decode属性
 
@@ -59,7 +52,6 @@ module.exports = [
   // 缺失属性  百度中无space属性
 
   // progress
-  // 缺失属性  百度中无border-radius、font-size、aria-label属性
   // 缺失事件  百度中无bindactiveend事件
   // 注意事项
   // 1.该组件属性stroke-width的类型在微信小程序中是Number / String,默认值是6,单位是px（2.4.0起支持rpx）,在百度小程序中是Number,默认值是2,单位是px
@@ -70,8 +62,6 @@ module.exports = [
   //// 表单组件
   // button
 
-  // 缺失属性  百度中无lang、session-from、send-message-title、send-message-path、send-message-img、show-message-card
-  // app-parameter、aria-label属性
   // 缺失事件   百度中无bindcontact、binderror、bindlaunchapp事件
   // 注意事项
   // 1.该组件属性open-type 有效值：launchApp (打开APP，可以通过app-parameter属性设定向APP传的参数具体说明)、feedback (打开“意见反馈”页面，用户可提交反馈内容并上传日志，
@@ -86,7 +76,6 @@ module.exports = [
   // 1.该组件bindsubmit返回值微信和百度不太一样,微信是event.detail = {value : {'name': 'value'} , formId: ''},百度是event.detail = {value : {'name': 'value'}}
 
   // input
-  // 属性缺失  百度中无auto-focus、aria-label属性
   // 属性差异
   // 1.该组件属性cursor-spacing在微信小程序中单位是px（2.4.0起支持rpx）,类型是Number / String;百度小程序中是px,类型是Number
   // 2.该组件属性type有效值百度小程序中无idcard(身份证输入键盘)
@@ -107,7 +96,6 @@ module.exports = [
   // 其中字段code是统计用区划代码，postcode是邮政编码百度中是event.detail = {value: value}
 
   // picker-view
-  // 缺失属性  百度中无aria-label属性
   // 缺失事件  百度中无bindpickstart	、bindpickend事件
   // 注意事项  微信中该组件滚动时在iOS自带振动反馈，可在系统设置 -> 声音与触感 -> 系统触感反馈中关闭
 
@@ -115,19 +103,16 @@ module.exports = [
   // 缺失属性  百度中无aria-label属性
 
   // slider
-  // 缺失属性  百度中无color、aria-label属性
   // 差异属性
   // 1.属性activeColor默认值在微信中是#1aad19,百度中是#3c76ff
   // 2.属性backgroundColor默认值在微信中是#e9e9e9,百度中是#cccccc
   // 3.属性block-size默认值在微信中是28,百度中是24
 
   // switch
-  // 缺失属性  百度中无aria-label属性
   // 差异事件  bindchange事件返回值在微信中是event.detail={ value:checked},百度中是event.detail={ checked:true}
 
   // textarea
   // 差异属性  属性cursor-spacing在微信中类型是Number / String,单位px（2.4.0起支持rpx）;百度中类型是Number,单位是px
-  // 缺失属性  百度中无aria-label属性
   // 差异事件  事件bindlinechange的返回值在微信中是event.detail = {value, cursor, keyCode}，keyCode 为键值，目前工具还不支持返回keyCode参数,百度
   // 中是event.detail = {value, cursor}
 
@@ -147,15 +132,12 @@ module.exports = [
 
   // image
 
-  // 缺失属性  百度中无aria-label属性
   // 注意事项
   // 1.属性lazy-load微信中说明是在即将进入当前屏幕可视区域时才开始加载,百度中是只针对 scroll-view 下的 image 有效
   // 2.微信中image组件中二维码/小程序码图片不支持长按识别。仅在wx.previewImage中支持长按识别,百度中文档未说明
 
   // video
 
-  // 缺失属性  百度中无direction、show-mute-btn、title、play-btn-position、enable-play-gesture、auto-pause-if-navigate、auto-pause-if-open-native
-  // vslide-gesture-in-fullscreen属性
   // 差异属性  属性（当视频大小与 video 容器大小不一致时，视频的表现形式）微信中命名是object-fit,百度中命名是objectFit
   // 缺失事件  百度中无bindplay、bindprogress事件
   // 注意事项
@@ -164,13 +146,11 @@ module.exports = [
 
   //camera
 
-  // 缺失属性  百度中无mode属性
   // 缺失事件  百度中无bindscancode事件
   // 注意事项  组件Bug & Tip说明部分微信和百度中不完全一样
 
   // live-player
 
-  // 缺失属性  百度中无mode、sound-mode、auto-pause-if-navigate	、auto-pause-if-open-native属性
   // 差异属性
   // 1.属性src在微信中仅支持 flv, rtmp 格式,百度中仅支持m3u8 格式
   // 2.属性background-mute在微信中已废弃
@@ -184,7 +164,6 @@ module.exports = [
   //// 地图
 
   // map
-  // 缺失属性  百度中无subkey属性
   // 差异属性
   // 1.属性show-compass、polygons、enable-overlooking	、enable-zoom、enable-scroll、enable-rotate百度中工具暂不支持
 
@@ -218,7 +197,6 @@ module.exports = [
 
   // open-data
 
-  //  缺失属性  百度中无open-gid、lang属性
   //  差异属性  属性type有效值在百度和微信中不完全一样
   // 1.百度中是userNickName(userNickName)、userAvatarUrl(用户图像)、userGender(用户性别)
   // 2.微信中是groupName(拉取群名称)、userNickName(用户昵称)、userAvatarUrl(用户头像)、userGender(用户性别)、
