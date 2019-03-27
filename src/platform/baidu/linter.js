ac72743b4e86c80ef43339156d532c6000392f40
chenyu15 <chenyu15@58ganji.com>
2019-03-27 15:46:16 +0800

    百度增加检测js

--- src/platform/baidu/linter.js
+++ src/platform/baidu/linter.js
@@ -4,6 +4,7 @@ const commonLint = require('../../utils/linter');
 function lint(opt = {}) {
   const sourcePath = opt.source || './src';
   commonLint('百度', `${sourcePath}/**/*.wxml`, linter.wxmlFileRules, linter.wxmlLineRules);
+  commonLint('百度', `${sourcePath}/**/*.js`, linter.scriptFileRules, linter.scriptLineRules);
 }
 
 module.exports = lint;
