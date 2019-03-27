410be04e3dc5adaaf0a21b8a3df12e29b8195929
chenyu15 <chenyu15@58ganji.com>
2019-03-27 11:15:21 +0800

    修改ali linter button 检测

--- src/linter/src/utils/utils.js
+++ src/linter/src/utils/utils.js
@@ -24,7 +24,8 @@ function unsupportedAttrOrEvents(contents, path, coms, platform, type) {
 
   coms.forEach((com) => {
     com.attrs.forEach((attr) => {
-      const reg = new RegExp(`<${com}[^>]+${attr}=`);
+
+      const reg = new RegExp(`<${com.tag}[^>]*${attr}=`);
       const match = contents.match(reg);
 
       if (match) {
